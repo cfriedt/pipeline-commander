e43a0ac9018ad44a349a623156e82704341058f7
Christopher Friedt <chrisfriedt@gmail.com>
2017-09-27 15:39:36 +0000

    Update pipeline-commander.py
    * Added proper handling of '--verbose' argument.
    * added get_build_status_webpage() so the user can just click on the link to follow updates
    * Trimmed whitespace

--- pipeline-commander.py
+++ pipeline-commander.py
@@ -3,16 +3,26 @@
 import argparse
 import json
 import os
+import pprint
 import requests
 import sys
 import time
 
+# https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def str2bool(v):
+	if v.lower() in ('yes', 'true', 't', 'y', '1'):
+		return True
+	elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
+
 class pipeline_commander:
 	"""
 	Pipeline Commander
-	
+
 	A hackish tool to trigger a GitLab pipeline and wait for its completion.
-	
+
 	See https://docs.gitlab.com/ee/api/pipelines.html
 	"""
 
@@ -38,11 +48,12 @@ class pipeline_commander:
 		self.server_url = server_url
 		self.git_ref = git_ref
 		self.project_id = project_id
+		self.project_jsn = None
 		self.verbose = verbose
-		
+
 		if private_token is not None:
 			self.private_token = private_token
-			
+
 		if trigger_token is not None:
 			self.trigger_token = trigger_token
 
@@ -68,18 +79,40 @@ class pipeline_commander:
 			raise ValueError( response )
 
 		self.triggered = True
-		self.trigger_jsn = json.loads( response.text )
+		jsn = json.loads( response.text )
+		self.build_jsn = jsn
 
-		self.v( "Trigger Response:\n{0}".format( response.text ) )
-		self.v( "Build status URL is {0}".format( self.get_pipeline_url() ) )
+		self.v( "Trigger Response:\n{0}".format( pprint.pformat( jsn, indent = 4 ) ) )
+
+		print( '=' * 80 )
+		print( self.get_build_status_webpage() )
+		print( '=' * 80 )
 
 
-	def get_pipeline_url( self ):
+	def get_build_status_webpage( self ):
+
+		self.get_project_info()
+
+		url = "{0}/pipelines/{1}".format( self.project_jsn[ 'web_url' ], self.build_jsn[ 'id' ] )
+
+		return url
+
+
+	def get_build_url( self ):
 
 		if not self.triggered:
 			ValueError( 'Not triggered' )
 
-		url = "{0}/api/v4/projects/{1}/pipeline/{2}".format( self.server_url, self.project_id, self.trigger_jsn[ 'id' ] )
+		url = "{0}/api/v4/projects/{1}/pipeline/{2}".format( self.server_url, self.project_id, self.build_jsn[ 'id' ] )
+
+		return url
+
+	def get_build_url( self ):
+
+		if not self.triggered:
+			ValueError( 'Not triggered' )
+
+		url = "{0}/api/v4/projects/{1}/pipeline/{2}".format( self.server_url, self.project_id, self.build_jsn[ 'id' ] )
 
 		return url
 
@@ -97,6 +130,33 @@ class pipeline_commander:
 
 		return url
 
+	def get_projects_url( self, project_id = None ):
+
+		if project_id is None:
+			url = "{0}/api/v4/projects".format( self.server_url )
+		else:
+			url = "{0}/api/v4/projects/{1}".format( self.server_url, self.project_id )
+
+		return url
+
+	def get_project_info( self ):
+
+		if self.project_jsn is None:
+
+			headers = { 'PRIVATE-TOKEN': self.private_token }
+
+			url = self.get_projects_url( self.project_id )
+
+			self.v( "Getting Project via URL {0}".format( url ) )
+
+			response = requests.get( url, headers = headers )
+
+			jsn = json.loads( response.text )
+			self.project_jsn = jsn
+
+			self.v( "Project Info:\n{0}".format( pprint.pformat( jsn, indent = 4 ) ) )
+
+		return self.project_jsn
 
 	def get_status( self ):
 
@@ -106,39 +166,40 @@ class pipeline_commander:
 		headers = { 'PRIVATE-TOKEN': self.private_token }
 
 		url = self.get_jobs_url()
-		
-		self.v( "Getting Build ID {0} for Project ID {1} via URL {2}".format( self.trigger_jsn[ 'id' ], self.project_id, url ) )
-		
+
+		self.v( "Getting Status via URL {0}".format( url ) )
+
 		response = requests.get( url, headers = headers )
 
 		jsn = json.loads( response.text )
 
 		for e in jsn:
-			if e[ 'id' ] == self.trigger_jsn[ 'id' ]:
+			if e[ 'id' ] == self.build_jsn[ 'id' ]:
+				self.v( "Job Status:\n{0}".format( pprint.pformat( e, indent = 4 ) ) )
 				return e[ 'status' ]
 
-		raise ValueError( "Build id {0} for project id {1} not found!".format( self.trigger_jsn[ 'id' ], self.project_id ) )
-	
+		raise ValueError( "Build ID {0} for Project ID {1} not found!".format( self.build_jsn[ 'id' ], self.project_id ) )
+
 
 	def wait_for( self ):
-		
+
 		prev_status = ''
-		
+
 		while True:
-			
+
 			status = self.get_status()
-			
+
 			if status != prev_status:
 				print( "status: {0}".format( status ) )
 				prev_status = status
-				
+
 			if "success" == status:
 				return 0
-			
+
 			elif "failed" == status:
 				print( "\nError:\n{0}".format( status_jsn ) )
 				return 1
-			
+
 			else:
 				sys.stdout.write( '.' )
 				sys.stdout.flush()
@@ -151,23 +212,25 @@ class pipeline_commander:
 if __name__ == '__main__':
 
 	ap = argparse.ArgumentParser( description = 'Trigger a GitLab Pipeline and wait for its completion' )
-	
+
 	ap.add_argument( '-r', '--git-ref', help = 'Git reference, e.g. master' )
 	ap.add_argument( '-o', '--timeout', type = int, help = 'Timeout in seconds' )
 	ap.add_argument( '-i', '--project-id', type = int, help = 'Numerical Project ID (under Settings->General Project Settings in GitLab' )
 	ap.add_argument( '-p', '--private-token', help = 'An private or personal token authorised to query pipeline status. See https://docs.gitlab.com/ee/api/README.html#private-tokens. By default, this value is initialized with PRIVATE_TOKEN environment variable.' )
 	ap.add_argument( '-u', '--server-url', help = 'Server URL to use, e.g. http://localhost:80' )
 	ap.add_argument( '-t', '--trigger-token', help = 'The trigger token for a pipeline. See https://docs.gitlab.com/ee/ci/triggers. By default, this value is initialized with TRIGGER_TOKEN environment variable.' )
-	ap.add_argument( '-v', '--verbose', help = 'Print more verbose information' )
+	ap.add_argument( '-v', '--verbose', type = str2bool, nargs = '?', const = True, help = 'Print more verbose information' )
 
 	args = ap.parse_args()
 
 	pc = pipeline_commander()
-	
+
 	for key in vars( args ):
 		val = getattr( args, key )
 		if val is not None:
 			setattr( pc, key, val )
+
+	pc.get_project_info()
 
 	pc.trigger()
 	exit( pc.wait_for() )
